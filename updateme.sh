b46a61c96584f3039b8cec0288350d317b222840
Takashi Doyama <63372896+donchan91@users.noreply.github.com>
2021-12-03 15:00:05 +0000

    Initial File
    Displays what command is run, and runs it.

---
+++ updateme.sh
@@ -0,0 +1,7 @@
+echo "[*] Running: sudo apt-get update -y"
+sudo apt-get update -y
+echo "[*] Running: sudo apt-get full-upgrade -y"
+sudo apt-get full-upgrade -y
+echo "[*] Running: sudo apt autoremove -y"
+sudo apt autoremove -y
+echo "[*] Updates complete."
