4dfa97dbbeef243e5e3506410dd8d1483b10db9b
Takashi Doyama <63372896+donchan91@users.noreply.github.com>
2023-03-20 21:38:47 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-echo "Copying updateme.sh into /usr/local/bin"
-sudo cp updateme.sh /usr/local/bin/updateme.sh 
+echo "Copying updateme.sh into /opt/updateme"
+sudo mkdir /opt/updateme
+sudo cp updateme.sh /opt/updateme/updateme.sh 
 echo "Setting running permissions"
-sudo chmod +x /usr/local/bin/updateme.sh 
+sudo chmod +x /opt/updateme/updateme.sh 
 echo "Setting aliases"
-echo 'alias updateme="/usr/local/bin/updateme.sh"' >> ~/.bashrc
-echo 'alias updateme="/usr/local/bin/updateme.sh"' >> ~/.zshrc
+echo 'alias updateme="/opt/updateme/updateme.sh"' >> ~/.bashrc
+echo 'alias updateme="/opt/updateme/updateme.sh"' >> ~/.zshrc
+sudo echo 'alias updateme="/opt/updateme/updateme.sh"' >> /root/.bashrc
+sudo echo 'alias updateme="/opt/updateme/updateme.sh"' >> /root/.zshrc
+
 echo "Done!! Restart the terminal to reflect changes on aliases."
